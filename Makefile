0d8b72aac26aad0629479057a0123c35b52d034b
Sandra Kanna <skanna@k1r2p7.42mulhouse.fr>
2024-01-22 13:36:59 +0100

    preparing

---
+++ Makefile
@@ -0,0 +1,35 @@
+NAME = push_swap
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror -g3
+#-fsanitize=address 
+#-Ibonus
+
+CFILES = src/push_swap.c utils/utils.c
+
+#BONUS_FILES = bonus/bonus.c bonus/bonus-utils.c \
+                        gnl/get_next_line_utils.c gnl/get_next_line.c
+
+OBJ = $(CFILES:.c=.o)
+#BONUS_OBJ = $(BONUS_FILES:.c=.o)
+
+%.o: %.c
+        $(CC) $(CFLAGS) -c $< -o $@
+
+$(NAME): $(OBJ)
+        $(CC) $(CFLAGS) $(CFILES) -o $(NAME)
+
+all: $(NAME)
+
+clean:
+        rm -f $(OBJ) 
+#$(BONUS_OBJ)
+
+fclean: clean
+        rm -f $(NAME)
+
+re: fclean all
+
+#bonus: clean
+#$(CC) $(CFLAGS) $(OBJ) $(BONUS_FILES) -o $(NAME)
+
+.PHONY: all clean fclean re bonus
---
+++ utils.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skanna <skanna@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 13:06:48 by skanna            #+#    #+#             */
+/*   Updated: 2024/01/22 13:27:34 by skanna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	sa()
+{
+	//Do nothing if there is only one or no elements: if node1 or node 2 == NULL
+	//sa (swap a): Swap the first 2 elements at the top of stack a.
+
+}
+
+void	sb()
+{
+	//Do nothing if there is only one or no elements: if node1 or node 2 == NULL
+	//sb (swap b): Swap the first 2 elements at the top of stack b.
+
+}
+
+void	ss()
+{
+	//ss: sa and sb at the same time.
+
+}
+
+void	pa()
+{
+	//Do nothing if b is empty.
+	//pa (push a): Take the first element at the top of b and put it at the top of a.
+}
+
+void	pb()
+{
+	//Do nothing if b is empty.
+	//pb (push b): Take the first element at the top of a and put it at the top of b.
+}
+
+void	ra()
+{
+	//(rotate a): Shift up all elements of stack a by 1.
+	//The first element becomes the last one.
+}
+
+void	rb()
+{
+	//rb (rotate b): Shift up all elements of stack b by 1.
+	//The first element becomes the last one.
+}
+
+void	rr()
+{
+	//rr : ra and rb at the same time.
+}
+
+void	rra()
+{
+	//rra (reverse rotate a): Shift down all elements of stack a by 1.
+	//The last element becomes the first one.
+}
+
+void	rra()
+{
+	//rrb (reverse rotate b): Shift down all elements of stack b by 1.
+	//The last element becomes the first one.
+}
+
+void	rra()
+{
+	//rrr : rra and rrb at the same time.
+}
