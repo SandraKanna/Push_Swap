411f6b6d5ed1415490a25d748d3c44325c737101
Sandra Kanna <sandrakanna@gmail.com>
2024-01-24 23:40:13 +0100

    tests OK for: input, classic lists and circular lists basic operations

---
+++ tests/test_classic.c
@@ -0,0 +1,49 @@
+#include "../src/push_swap.h"
+
+void	print_test(t_node *printme)
+{
+	while (printme != NULL)
+	{
+		printf("%i\n", printme->value);
+		printme = printme->next;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	char	**new_tab = NULL;
+	int		ac;
+	int		add = 44;
+	t_node	*stack_test;
+
+	new_tab = parse_args(&ac, argc, argv);
+	//test create stack
+	printf("Initializing the stack A:\n---\n");
+	stack_test = initialize_stack_a(new_tab, ac);
+	print_test(stack_test);
+	printf("\n");
+
+	//test push
+	printf("Add: %i\n---\n", add);
+	push1(&stack_test, add);
+	print_test(stack_test);
+	printf("\n");
+
+	//test pop
+	printf("Remove: %i\n---\n", pop1(&stack_test));
+	print_test(stack_test);
+	printf("\n");
+
+	//test swap
+	printf("Swap:\n---\n");
+	swap1(&stack_test);
+	print_test(stack_test);
+	printf("\n");
+/*
+	//test rotate
+	printf("Rotate:\n---\n");
+
+	print_test(stack_test);
+	printf("\n");*/
+	return 0;
+}
---
+++ tests/test_input.c
@@ -0,0 +1,81 @@
+#include "../src/push_swap.h"
+
+void	test_parse_args(int *argc2, char **argv2, char ***new_tab)
+{
+	int		ac_test;
+	int		i = 0;
+
+	printf("initial argc: %i\n", *argc2);
+	while (argv2[i])
+	{
+		printf("arg[%i}: %s\n", i, argv2[i]);
+		i++;
+	}
+	i = 0;
+	*new_tab = parse_args(&ac_test, *argc2, argv2);
+	*argc2 = ac_test;
+	printf("new argc: %i\n", *argc2);
+	while ((*new_tab)[i])
+	{
+		printf("new arg[%i}: %s\n", i, (*new_tab)[i]);
+		i++;
+	}
+}
+
+void	test_digits(int test_argc, char **test_argv)
+{
+	int	i = 0;
+
+	while (i < test_argc)
+	{
+		if (!ft_isdigit(test_argv[i]))
+		{
+			printf("argv[%i] = %s is not a digit\n", i, test_argv[i]);
+			exit (EXIT_FAILURE);
+		}
+		i++;
+	}
+	printf("All inputs are digits\n");
+}
+
+void	test_convert(int *ac, char **av, int **convert)
+{
+	int	err = 0;
+	int	i = 0;
+
+	while (i < *ac)
+	{
+		(*convert)[i] = do_atoi(av[i], &err);
+		printf("%s was converted to int %i\n", av[i], (*convert)[i]);
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int		*convert;
+	char	**new_tab = NULL;
+
+	printf("---Arguments parsing---\n");
+	test_parse_args(&ac, av, &new_tab);
+	printf("\n");
+
+	printf("---Checking digits---\n");
+	test_digits(ac, new_tab);
+	printf("\n");
+
+	printf("---Converting to int---\n");
+	convert = malloc (sizeof(int) * (ac));
+	if (!convert)
+	{
+		printf("Malloc error");
+		exit(1);
+	}
+	test_convert(&ac, new_tab, &convert);
+	if (has_duplicates(ac, convert))
+		printf("There are duplicates in the input\n");
+	else
+		printf("There are no duplicates in the input\n");
+	free(convert);
+	printf("\n");
+}
